c3fa1b944b8ceccc4e0ddcb2e4523e6913dfafdb
Lznah <hanzlpe@icloud.com>
2021-01-30 19:34:24 +0100

    Added comments that describes envvars in run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# path to flask app
 export FLASK_APP='scanbrokers/server.py'
+# path to json data folder
 export WEBSERVER_DATAPATH='./data/'
+# run in debug mode? Yes - 1; No - 0
 export FLASK_DEBUG=1
 flask run
